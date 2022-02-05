3070e74bcda89cc48be2f84bbd42023a428641b0
MrAlexGert <alex.gert1812@.gmail.com>
2022-02-05 14:47:33 +0300

    auth

--- amo/urls.py
+++ amo/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from crm.views import ApiView
+from crm.views import ApiView, auth, TestApi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', ApiView.as_view())
+    path('api/v1/', ApiView.as_view()),
+    # path('api/v1/', TestApi.as_view()),
+    path('', include('social_django.urls', namespace='social')),
+    path('auth/', auth)
 ]
