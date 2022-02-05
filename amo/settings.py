cff7ddc2d178f155fd102ee11c526718cb28e05e
MrAlexGert <alex.gert1812@.gmail.com>
2022-02-05 17:30:58 +0300

    fix

--- amo/settings.py
+++ amo/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lmylaccno%t=nv)sb$&f14*p&=vkdsxh=n#_%_rq09yj)eog0-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
