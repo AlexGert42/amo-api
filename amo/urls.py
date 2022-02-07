bf397cee7d002f86bf6fd007a8bd01477ec0b077
AlexGert42 <alex.gert1812@gmail.com>
2022-02-07 19:15:00 +0300

    auth

--- amo/urls.py
+++ amo/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
 
-from crm.views import ApiView, auth
+from crm.views import ApiView, auth, AuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', ApiView.as_view()),
     path('', include('social_django.urls', namespace='social')),
     path('auth/', auth),
-    path('token-auth/', views.obtain_auth_token)
+    path('token-auth/', AuthToken.as_view())
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,9 @@
 import json
 
+from django.http import HttpRequest
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -20,6 +23,21 @@ class ApiView(APIView):
 
         return Response({
             select['data']: json.loads(result.content.decode('utf-8'))
+        })
+
+
+class AuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        print(request.ParseError)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
         })
 
 
